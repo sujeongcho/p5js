d4cc304a42a4182eaa337183d78604b56a70b019
Sujeong Cho <112532259+sujeongcho@users.noreply.github.com>
2023-01-10 16:27:37 +0900

    Create ifelse.js

---
+++ ifelse.js
@@ -0,0 +1,23 @@
+let brushSize = 50;
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  
+}
+
+function mouseDragged(){
+  if(mouseX < width/2){
+    brushSize = 25;
+    fill(random(255), random(255), random(255));
+  }else{
+    brushSize = 50;
+    fill(random(255));
+  } if(mouseY <height/2){
+    rect(mouseX,mouseY, brushSize);
+  }else{
+     ellipse(mouseX, mouseY, brushSize )};
+}
