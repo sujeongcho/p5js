166aa204c140871b0df53d576a143cc259d6b2a5
Sujeong Cho <112532259+sujeongcho@users.noreply.github.com>
2023-01-11 19:40:00 +0900

    Update text_particle.js

--- js/text_particle.js
+++ js/text_particle.js
@@ -1,12 +1,13 @@
 const fontSize = 44,
- scaleRate = 6, 
- msg1 = 'DAEGU ART MUSEUM',
- inpactRange = fontSize*scaleRate/1;
+    scaleRate = 6,
+    msg1 = 'DAEGU ART MUSEUM',
+    inpactRange = fontSize * scaleRate / 1;
 let canvas;
 let textData = [];
 let dotsCordinate = [];
-let particles =[];
+let particles = [];
 let lato;
+let slider;
 
 class Particle {
     constructor(x, y) {
@@ -15,8 +16,8 @@ class Particle {
         this.r = 3;
         this.originalX = x;
         this.originalY = y;
-        this.color = Math.floor(Math.random()*280);
-        this.density = Math.random() * 30 + 10;
+        this.color = Math.floor(Math.random() * 280);
+        this.density = Math.random() * 20 + 10;
         // console.log(this.fillStyle);
     }
 
@@ -61,7 +62,7 @@ class Particle {
 // **************************************
 
 
-function preload(){
+function preload() {
 
     lato = loadFont('../fonts/ENGINE.ttf')
 }
@@ -91,26 +92,51 @@ function setup() {
     particles = createParticles(scaleRate, 10, 300);
     // console.log(dotsCordinate)
     // console.log(particles)
+
+
+    // //button
+    // let btn = createButton("Click!");
+    // btn.position(width / 2, 100);
+    // btn.style("background", "black")
+    // btn.style("color", "white")
+    // btn.style("font-size", "40px")
+    // btn.size(200, 70);
+
+    //slider
+    slider = createSlider(0, 255, 100);
+    slider.position(10, height - 30);
+    slider.size(width - 20, 20);
+    slider.style("background","transparent")
+    slider.style("-webkit-appearance","none")
+    slider.style("outline","none")
+    slider.style("cursor","pointer")
+
 }
+
+
+
+
+
+
 
 
 function draw() {
     // noLoop();
-    background("black");
+    background(slider.value());
     updating();
     drawParticles();
 }
 
-function mouseDragged(){
+function mouseDragged() {
 
 }
 
 function getTextData(msg1) {
     const data = [];
     text(msg1, 0, 0);    // draw once and get data
-    for(let y = 0; y < textAscent(msg1); y++){
+    for (let y = 0; y < textAscent(msg1); y++) {
         let row = [];
-        for(let x = 0; x < textWidth(msg1); x++){
+        for (let x = 0; x < textWidth(msg1); x++) {
             row.push(canvas.get(x, y))    // get data, [r, g, b, a]
         }
         data.push(row);
@@ -126,7 +152,7 @@ function getCordinates() {
             let red = textData[y][x][0];    // the data equals [0, 0, 0, 255] or [255, 255,255, 255]. So pick up red value and judge
             if (red < 128) {    // if < 128, regard the pixel as 'black'(1);
                 row.push(1);
-            }else{
+            } else {
                 row.push(0);
             }
         }
@@ -138,8 +164,8 @@ function getCordinates() {
 function createParticles(scaleRate, marginX, marginY) {
     const particles = [];
     for (let y = 0; y < dotsCordinate.length; y++) {
-        for(let x = 0; x < dotsCordinate[0].length; x++){
-            if(dotsCordinate[y][x] === 1){
+        for (let x = 0; x < dotsCordinate[0].length; x++) {
+            if (dotsCordinate[y][x] === 1) {
                 let particle = new Particle(x * scaleRate + marginX, y * scaleRate + marginY);
                 particles.push(particle)
             }
